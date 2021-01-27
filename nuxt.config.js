1330a1e96fb08e703ca98532058d9260526b9037
hiroaki-1017 <yh111017@outlook.jp>
2021-01-28 00:51:41 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,58 @@
-
 export default {
-  mode: 'spa',
+  target: "static",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
+  ssr: false,
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/icon?family=Material+Icons"
+      }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-    '~/assets/css/tailwind.css'
-  ],
+   ** Global CSS
+   */
+  css: ["~/assets/css/tailwind.css"],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '~/plugins/firebase',
-    '~/plugins/auth',
-    '~/plugins/user'
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: ["~/plugins/firebase", "~/plugins/auth", "~/plugins/user"],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss"
   ],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
+   ** Nuxt.js modules
+   */
+  modules: [],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
